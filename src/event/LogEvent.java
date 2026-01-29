b9f8df2bf5b64fe7758d19d16545861a3b7897ce
Nathaniel Odion <nathanilo@users.noreply.github.com>
2026-01-29 21:13:50 +0100

    Implement immutable LogEvent

---
+++ src/event/LogEvent.java
@@ -0,0 +1,87 @@
+package event;
+
+import java.util.Date;
+import java.util.EventObject;
+
+
+import model.Operation;
+import model.Priority;
+import model.Status;
+
+/**
+ * LogEvent represents a single log entry as an immutable event -
+ * It represent past actions and should not be modified by listeners once created..
+ */
+
+public class LogEvent extends EventObject implements ILogEvent {
+    private final Date date;
+    private final String user;
+    private final Operation operation;
+    private final Priority priority;
+    private final String node;
+    private final String resource;
+    private final Status status;
+    private final int latency;
+
+    public LogEvent(
+            Object source,
+            Date date,
+            String user,
+            Operation operation,
+            Priority priority,
+            String node,
+            String resource,
+            Status status,
+            int latency
+    ) {
+        super(source);
+        this.date = date;
+        this.user = user;
+        this.operation = operation;
+        this.priority = priority;
+        this.node = node;
+        this.resource = resource;
+        this.status = status;
+        this.latency = latency;
+    }
+
+    @Override
+    public Date getDate() {
+        return date;
+    }
+
+    @Override
+    public String getUser() {
+        return user;
+    }
+
+    @Override
+    public Operation getOperation() {
+        return operation;
+    }
+
+    @Override
+    public Priority getPriority() {
+        return priority;
+    }
+
+    @Override
+    public String getNode() {
+        return node;
+    }
+
+    @Override
+    public String getResource() {
+        return resource;
+    }
+
+    @Override
+    public Status getStatus() {
+        return status;
+    }
+
+    @Override
+    public int getLatency() {
+        return latency;
+    }
+}
